50c49678f9504f2f25d36674084c8885308bb6a2
Halim Wagih <halemwagih6@gmail.com>
2024-08-23 16:37:52 +0300

    Task management layout division

--- lib/features/home_layout/presentation/manager/home_layout_cubit/home_layout_cubit.dart
+++ lib/features/home_layout/presentation/manager/home_layout_cubit/home_layout_cubit.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_management_app/features/profile/presentation/profile_layout/view/profile_layout_view.dart';
 import '../../../../../core/utils/widgets/duration.dart';
-import '../../../../profile/presentation/home_layout_profile/view/home_layout_profile_view.dart';
 import '../../../../support/view/support_view.dart';
 import 'home_layout_state.dart';
 
 class HomeLayoutCubit extends Cubit<HomeLayoutState>{
   late PageController pageController;
   List<Widget> taps = [
-    const HomeLayoutProfileView(),
+    const ProfileLayoutView(),
     Container(
       color: Colors.green,
     ),
